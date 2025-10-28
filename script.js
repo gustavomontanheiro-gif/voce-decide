43a5877870de6023915cfdfe1ca4a3a20affb358
gustavomontanheiro-gif <gustavo.montanheiro@escola.pr.gov.br>
2025-10-28 08:33:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const avanca = document.querySelectorA11(".btn-proximo")
+
+avanca.forEach(button) => {
+  button.addEventListener('click',function(){
+    const atual = document.querySelector
+  }
+}
